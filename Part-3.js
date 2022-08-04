ca49cfd3195cd21ad27715c7029f3868ae0f4060
CodeWithSachin <sachinnnsingh0510@gmail.com>
2022-08-04 22:44:37 +0530

    setInterval

--- Part-3.js
+++ Part-3.js
@@ -15,15 +15,19 @@
 
 // ====================== setInterval ======================
 
-const body = document.body;
-const button = document.querySelector("button");
-const intertvalId = setInterval(()=> {
-        const red = Math.floor(Math.random() * 126);
-        const green = Math.floor(Math.random() * 126);
-        const blue = Math.floor(Math.random() * 126);
+// const body = document.body;
+// const button = document.querySelector("button");
+// const intertvalId = setInterval(()=> {
+//         const red = Math.floor(Math.random() * 126);
+//         const green = Math.floor(Math.random() * 126);
+//         const blue = Math.floor(Math.random() * 126);
 
-        const rgb = `rgb(${red},${green},${blue})`;
+//         const rgb = `rgb(${red},${green},${blue})`;
 
-        body.style.background = rgb;
-}, 1000);
+//         body.style.background = rgb;
+// }, 1000);
 
+// button.addEventListener("click", () => {
+//         clearInterval(intertvalId);
+//         button.textContent = body.style.background;
+// })
