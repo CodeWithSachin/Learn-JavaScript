cc7ee58670befee7cd1c2cc8fe2ea3f10ff2bb55
Sachin Singh <sachin.singh@mileseducation.com>
2023-07-31 17:00:51 +0530

    folder structure set and async JS initiated.

---
+++ Async_Javascript/main.js
@@ -0,0 +1,15 @@
+// what is synchronous(sync) and asynchronous(async)
+// is javascript is async?
+// Javascript is not async
+// story of async
+// single and multiple threading
+// callbacks
+// Promises
+// then catch
+// try catch
+// async await
+// 5 real world use case
+
+// [Concept Aside]
+// Concurrency and Parallelism
+// Throttling
